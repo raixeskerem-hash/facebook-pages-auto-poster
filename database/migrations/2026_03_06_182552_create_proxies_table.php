f4c20798a577478e9860c4e7e2521133ec6584fb
raixeskerem-hash <raixeskerem@gmail.com>
2026-03-06 21:26:02 +0300

    Add migration for creating proxies table

---
+++ database/migrations/2026_03_06_182552_create_proxies_table.php
@@ -0,0 +1,33 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateProxiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proxies', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip_address')->unique();
+            $table->integer('port');
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proxies');
+    }
+}
