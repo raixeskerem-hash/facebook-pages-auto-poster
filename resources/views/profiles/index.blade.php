a2ff1cd39ecfbde3ab888ac8df7fd8e80cb85a52
raixeskerem-hash <raixeskerem@gmail.com>
2026-03-06 21:47:12 +0300

    Create index.blade.php for profiles listing page

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profiles Listing</title>
+</head>
+<body>
+    <h1>Profiles Listing</h1>
+    <ul>
+        <!-- Example data, replace with actual profile data -->
+        <li><a href="#">Profile 1</a></li>
+        <li><a href="#">Profile 2</a></li>
+        <li><a href="#">Profile 3</a></li>
+    </ul>
+</body>
+</html>
